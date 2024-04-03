9b3da4c5a20bcf7b95f8b28a32121c6ab41f38ec
Kukesan <kkukesan@gmail.com>
2024-04-03 12:25:01 +0530

    neo bank created

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Neo-bank-frontend'`, () => {
+  it(`should have as title 'neo-bank'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Neo-bank-frontend');
+    expect(app.title).toEqual('neo-bank');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Neo-bank-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('neo-bank app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Neo-bank-frontend';
+  title = 'neo-bank';
 }
